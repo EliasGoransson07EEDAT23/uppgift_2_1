a2bcb56afee6bd4005d8f398e5a9fa6a8fd3a815
elgo07001 <elgo07001@E-PW05C37M>
2024-10-09 11:59:41 +0200

    Add project files.

---
+++ uppgift_2_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace uppgift_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej, snabb fråga. Vilken är din favoritbok? Skriv titeln nedan!:");
+            string book = Console.ReadLine();
+            Console.WriteLine("Okej, så din favoritbok är alltså " + book + ".");
+            Console.ReadKey();
+
+        }
+    }
+}
